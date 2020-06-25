06832bf009ed08af3e9b6b2e79eb2aa2c3ec6632
Garima Gaur <66511198+gaurgarima@users.noreply.github.com>
2020-06-25 11:20:05 +0530

    Update query_registration.sh

--- query_registration.sh
+++ query_registration.sh
@@ -1,4 +1,7 @@
+#!/usr/bin/env bash
+
 queryFile=$1
+dataset=$2
 libPath="./:./lib/*:./lib/jena/*:./lib/neo4j/*"
 javac -cp $libPath main/QueryRegistryEngine.java 
 javac -cp $libPath main/QueryRegistryEngine $queryFile
